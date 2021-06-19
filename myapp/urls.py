9e2ba244d889cb17ebb2230178d3c46c77f7faf0
Shaufi <shaufiman@gmail.com>
2021-06-19 19:16:54 +0800

    upload project

---
+++ myapp/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from myapp import views
+
+app_name = 'myapp'
+
+urlpatterns = [
+    path('', views.index,name = 'index'),
+    #book/2
+    path('book/<int:book_id>/', views.detail, name = 'detail'),
+    path('add/', views.add_book, name = 'add_book'),
+    path('update/<int:id>', views.update, name = 'update'),
+    path('delete/<int:id>/', views.delete, name = 'delete'),
+]
